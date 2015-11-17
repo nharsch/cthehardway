47b00e471a065fd245591e67f22f384273dd4f72
nharsch <nigelharsch@gmail.com>
2015-11-16 23:48:35 -0500

    finished 9

--- ex3.c
+++ ex3.c
@@ -7,8 +7,8 @@ int main()
   
   printf("I am %d years old.\n", age);
   printf("I am %d inches tall.\n", height);
-  printf("I am '%x' hex years old and '%x' hex inches tall", age, height);
-  printf("and this is what happens when you don't add a substition: %s");
+  printf("I am '%x' hex years old and '%x' hex inches tall", 
+      age, height);
   
   return 0;
 }
--- ex4.c
+++ ex4.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-  /* int age = 10; */
+  int age = 10;
   int height = 3;
 
-  printf("I am %d years old.\n");
+  printf("I am %d years old.\n", age);
   printf("I am %d inches tall.\n", height);
 
   return 0;
